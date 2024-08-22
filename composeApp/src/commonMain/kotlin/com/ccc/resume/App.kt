697d699b82d46656938cc218c7fa12f2e7ccd65a
moonggae <egchoi.dev@gmail.com>
2024-08-22 15:59:01 +0900

    common App 분리

---
+++ composeApp/src/commonMain/kotlin/com/ccc/resume/App.kt
@@ -0,0 +1,18 @@
+package com.ccc.resume
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun App() {
+    MaterialTheme {
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Hello, World")
+        }
+    }
+}
--- composeApp/src/wasmJsMain/kotlin/com/ccc/resume/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.ccc.resume
-
-class WasmPlatform {
-    val name: String = "Web with Kotlin/Wasm"
-}
-
-fun getPlatform() = WasmPlatform()
